a60cea318a4dea57acf2326c7d2b6e4a27ee9c1e
Purvesh <ind.purvesh@gmail.com>
2016-04-04 10:25:34 +1200

    people view

--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <h4 class="title">Project View</h4>
-
+    <div class="row"> 
+       <h4 class="title">{{ $people->first_name }} {{ $people->last_name }}</h4>
+       <p> {{ $people->home_phone }} </p>
+       <p> {{ $people->mobile_phone }} </p>
+       <p> {{ $people->email }} </p>
         <div class="col-md-12">
-            <div class="title">{{ $project->title }}</div>
-            <p>{{ $project->description }}</p>
-
             <div>
 
                 <!-- Nav tabs -->
@@ -18,21 +17,6 @@
                             Info
                         </a>
                     </li>
-                    <li role="presentation">
-                        <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">
-                            Task
-                        </a>
-                    </li>
-                    <li role="presentation" >
-                        <a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">
-                            Stages
-                        </a>
-                    </li>
-                    <li role="presentation" class="active">
-                        <a href="#people" aria-controls="settings" role="tab" data-toggle="tab">
-                            People
-                        </a>
-                    </li>
                 </ul>
 
                 <!-- Tab panes -->
@@ -40,15 +24,7 @@
                     <div role="tabpanel" class="tab-pane" id="home">
                         <h5>Info</h5>
                         
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="profile">
-                        <h5>Task</h5>
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="messages">
-                        <h5>Stages</h5>
-                    </div>
-                    <div role="tabpanel" class="tab-pane active" id="people">
-                        <h5>People</h5>
+                        
                     </div>
                 </div>
 
