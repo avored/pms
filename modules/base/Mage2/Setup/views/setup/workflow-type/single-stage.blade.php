ddaceadacf40988204ed2fc19b40eef755b4ddc8
Purvesh <ind.purvesh@gmail.com>
2016-12-01 17:17:14 +1300

    few updates for work flow stages

--- modules/base/Mage2/Setup/views/setup/workflow-type/single-stage.blade.php
+++ modules/base/Mage2/Setup/views/setup/workflow-type/single-stage.blade.php
@@ -1,3 +1,19 @@
 <div class="list-group-item">
-    {{ $workflowStage->name }}
+    <span class="">{{ $workflowStage->name }}</span>
+
+    <div class="pull-right">
+        <div class="dropdown">
+            <div class="dropdown-toggle" 
+                   id="dropdownMenu1" data-toggle="dropdown">
+                <span class="glyphicon glyphicon-option-vertical"></span>
+            </div>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Add Child Stage</a></li>
+                <li><a href="#">Add Next Stage</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Destroy</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
