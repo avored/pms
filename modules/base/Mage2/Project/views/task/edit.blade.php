746d358348e8a3b555cb49f905f9eec6a8a9f7ea
Purvesh <ind.purvesh@gmail.com>
2018-08-03 09:40:25 +1200

    deleted repository

--- modules/base/Mage2/Project/views/task/edit.blade.php
+++
@@ -1,7 +0,0 @@
-{!! Form::model($task, ['method' => "put",'route' => ['project-task.update', $task->id]]) !!}
-
-@include('mage2project::task._fields')
-<div class="form-group">
-    {!!  Form::submit('Save',['class' => 'btn btn-raised btn-primary']) !!}
-</div>
-{!! Form::close() !!}
--- modules/base/Mage2/Setup/views/project-priority/_fields.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="form-group {{ ($errors->has('name') ? ' has-error' :'') }}" >
-    {{ Form::label('name','Name') }}
-    {{ Form::text('name',null,['class' => 'form-control ' ]) }}
-    <p class="help-block">
-    {{ $errors->first('name') }}
-    </p>
-</div>
