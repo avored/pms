a1557e3a1ab93958f674b2a3e9559c1f866c0935
Purvesh <ind.purvesh@gmail.com>
2019-02-06 14:41:25 +1300

    basic auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['verify' => true]);
+
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
